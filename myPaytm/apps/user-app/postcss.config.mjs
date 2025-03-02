a4b33eecc29a84ddb211abb79ec0264e1565ce8b
ADITYA RAJ <raj8017933@gmail.com>
2025-03-03 01:07:04 +0530

    updated 2

--- myPaytm/apps/user-app/postcss.config.mjs
+++ myPaytm/apps/user-app/postcss.config.mjs
@@ -1,4 +1,11 @@
 const config = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}"
+    ],
     plugins: {
         "@tailwindcss/postcss": {},
     },
